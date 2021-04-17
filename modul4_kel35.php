1f8e0b843d70c3ba1e7587db9f062ac3896cd513
MuhammadSulthonAuliya <msulthonauliya@students.undip.ac.id>
2021-04-18 05:21:04 +0700

    Catatan

--- modul4_kel35.php
+++ modul4_kel35.php
@@ -1,8 +1,3 @@
-
-//Function
-
-
-
 <?php
 	//ini return type
 	function hitung ($bil1, $bil2) {
@@ -27,44 +22,16 @@
 
 	echo "ini dikerjakan oleh function hitung2 <br>";
 	hitung2($bil1, $bil2);
- ?>
 
 
 
 
 
 
-//method
 
-<?php 
 
-	declare(strict_types = 1);
 
-	//class pendefinisi method
-	class pendefinisi {
 
-		public function akar(float $angka) : float {
-			return sqrt($angka);
-		}
-
-		public function staysafe (string $noun1, string $noun2) {
-			echo "Jangan lupa cuci tangan dengan $noun1 dan $noun2";
-		}
-
-	}
-
-	//code pemanggil method
-
-	$angka = 64;
-	//$objek merupakan instance dari class pendefinisi
-	$objek = new pendefinisi(); 
-	
-// OOP pada PHP menggunakan -> bukan . 
-	echo "akar dari $angka adalah ".$objek -> akar($angka);
-	echo "<hr>";
-	$objek -> staysafe('air', 'sabun');
-	
-?>
 
 
 
